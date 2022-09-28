4835bdb0c5fadefcb1b589d6b9b1ebb314049b33
Mikko Salomäki <ms@datarespons.se>
2022-09-28 10:24:22 +0200

    Enable gpio-aggregator for our "compatible"

--- drivers/gpio/gpio-aggregator.c
+++ drivers/gpio/gpio-aggregator.c
@@ -504,7 +504,8 @@ static const struct of_device_id gpio_aggregator_dt_ids[] = {
 	 * Add GPIO-operated devices controlled from userspace below,
 	 * or use "driver_override" in sysfs
 	 */
-	{}
+	{.compatible = "drs,system-gpios"},
+	{},
 };
 MODULE_DEVICE_TABLE(of, gpio_aggregator_dt_ids);
 #endif
